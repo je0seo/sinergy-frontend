09e24130500d306c01e7ea60cb50f99091352849
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2024-03-23 03:13:47 +0900

    최단경로 레이어~입력지 마커 생성 부분 코드 간소화 및 마커 클릭 이벤트 구현

--- src/components/MapC.js
+++ src/components/MapC.js
@@ -20,7 +20,7 @@ import { click } from 'ol/events/condition';
 
 import irumarkerS from './images/IrumakerS.png';
 import irumarkerE from './images/IrumakerE.png';
-import irumarker2 from './images/Irumarker2.png';
+import irumarker2 from './images/Irumaker2.png';
 
 
 const VWorldBaseUrl = 'https://api.vworld.kr/req/wmts/1.0.0/288AB3D7-7900-3465-BC2F-66917AB18D55';
@@ -63,10 +63,11 @@ const MapC = ({ pathData, width, height, keyword }) => {
         }),
         zIndex: 1
     });
-    const poiStyleS = (feature) => { //출발지 스타일
+
+    const basicMarkerStyle = (irumarker) => { //출발지 스타일
         return new Style({
             image: new Icon({
-                src: irumarkerS, 
+                src: irumarker,
                 scale: 0.1, // 이미지의 크기
                 opacity: 1, // 이미지의 투명도
                 rotateWithView: false, // 지도 회전에 따라 이미지를 회전할지 여부
@@ -74,28 +75,108 @@ const MapC = ({ pathData, width, height, keyword }) => {
             })
         });
     };
-    const poiStyle2 = (feature) => { //경유지 스타일
-        return new Style({
-            image: new Icon({
-                src: irumarker2, 
-                scale: 0.2, 
-                opacity: 1, 
-                rotateWithView: false, 
-                rotation: 0, 
-            })
-        });
+
+    const clickedMarkerStyle = (irumarker) => {
+      return new Style({
+          image: new Icon({
+              src: irumarker, // 이미지 파일의 경로를 설정합니다.
+              scale: 0.1, // 이미지의 크기를 조절합니다. 필요에 따라 조절하세요.
+              opacity: 0.7, // 이미지의 투명도를 조절합니다.
+              rotateWithView: false, // 지도 회전에 따라 이미지를 회전할지 여부를 설정합니다.
+              rotation: 0 // 이미지의 초기 회전 각도를 설정합니다.
+          })
+      });
     };
-    const poiStyleE = (feature) => {
-        return new Style({
-            image: new Icon({
-                src: irumarkerE, // 이미지 파일의 경로를 설정합니다.
-                scale: 0.1, // 이미지의 크기를 조절합니다. 필요에 따라 조절하세요.
-                opacity: 1, // 이미지의 투명도를 조절합니다.
-                rotateWithView: false, // 지도 회전에 따라 이미지를 회전할지 여부를 설정합니다.
-                rotation: 0 // 이미지의 초기 회전 각도를 설정합니다.
-            })
+
+    const addShortestPathLayer = (pathData) => {
+        pathData.forEach((path, index) => {
+            console.log(path);
+            const listOfEdgeId = path.map(e => e.edge);
+            const crsFilter = makeCrsFilter(listOfEdgeId);
+            // makeShortestPathLayer // 경로 지도레이어
+            const shortestPathLayer = new TileLayer({
+                title: `UOS Shortest Path ${index + 1}`,
+                source: new TileWMS({
+                    url: 'http://localhost:8080/geoserver/gp/wms',
+                    params: { 'LAYERS': 'gp:link', ...crsFilter },
+                    serverType: 'geoserver',
+                    visible: true,
+                    }),
+                zIndex: 2
+            });
+            map.addLayer(shortestPathLayer);
+        })
+    }
+    const makelocaArrayFromNodes = (pathData, locaArray) => {
+        pathData.forEach((path, index) => {
+            const listOfNodeId = path.map(n => n.node); // 주의: 출발지의 start_vid, 도착지의 end_vid는 빼고 node가 다 2개씩 있음
+            locaArray.push(listOfNodeId[0]);
+            if (index === pathData.length - 1) {
+                locaArray.push(listOfNodeId[listOfNodeId.length - 1]);
+            }
         });
-    };
+        return locaArray;
+    }
+
+    const setMarkerSrcOf = (locaArray,index) => {
+        if (index === 0) { // 출발지
+            return irumarkerS;
+        } else if (index === locaArray.length - 1) { //도착지
+            return irumarkerE;
+        } else {
+            return irumarker2; // 경유지
+        }
+    }
+
+    const markNodesFrom = (locaArray) => {
+        locaArray.forEach((nodeId, index) => {
+            // shortestPathLayer->nodeLayer
+            const nodeLayer = new VectorLayer({
+                title: `Marker ${index + 1}`,
+                visible: true,
+                source: new VectorSource({ // feature들이 담겨있는 vector source
+                    format: new GeoJSON({
+                        dataProjection: 'EPSG:5181'
+                    }),
+                    url: function(extent) { // WMS방식에서 WFS로 변경
+                        return 'http://localhost:8080/geoserver/gp/wfs?service=WFS&version=2.0.0' +
+                            '&request=GetFeature&typeName=gp%3Anode&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=node_id='+nodeId;
+                    },
+                    serverType: 'geoserver'
+                }),
+                style: basicMarkerStyle(setMarkerSrcOf(locaArray,index)),
+                zIndex: 5
+            });
+            map.addLayer(nodeLayer);
+        });
+    }
+
+    const markerClickEventWith = (locaArray) => {
+        //feature 클릭 가능한 select 객체
+        let selectSingleClick = new Select({
+           condition: click, // click 이벤트. condition: Select 객체 사용시 click, move 등의 이벤트 설정
+        });
+        map.addInteraction(selectSingleClick);
+
+        // feature를 선택할 때 이벤트
+        selectSingleClick.on('select', function(e) {
+            var selectedFeatures = e.selected;
+
+            selectedFeatures.forEach(function(feature) {
+                if (feature.get('node_id')==locaArray[0]) {
+                    feature.setStyle(clickedMarkerStyle(irumarkerS))
+                    console.log('출발지 click: '+feature.getId());
+                } else if (feature.get('node_id')==locaArray[locaArray.length-1]){
+                    feature.setStyle(clickedMarkerStyle(irumarkerE))
+                    console.log('도착지 click: '+feature.getId());
+                } else {
+                    feature.setStyle(clickedMarkerStyle(irumarker2))
+                    console.log('경유지 click: '+feature.getId());
+                }
+            });
+        });
+    }
+
 		//추가부분
     proj4.defs('EPSG:5181', '+proj=tmerc +lat_0=38 +lon_0=127 +k=1 +x_0=200000 +y_0=500000 +ellps=GRS80 +units=m +no_defs');
     register(proj4);
@@ -135,6 +216,7 @@ const MapC = ({ pathData, width, height, keyword }) => {
     useEffect(() => {
         if (map) {
             const layerExists = map.getLayers();
+            // 배경지도 옵션 설정
             if (layerExists) {
                 switch (layerState) {
                     case 'base-base':
@@ -154,78 +236,19 @@ const MapC = ({ pathData, width, height, keyword }) => {
                         break;
                 }
             }
-            // Add UOS Shortest Path layers
+
             var locaArray = []; // 출발, 경유지, 도착지의 link_id를 담는 배열
             // 출발지 도착지 다 분홍색 노드로 보여줬던 부분. 링크 추출
             if (pathData && pathData.length >= 1) { // 경로를 이루는 간선이 하나라도 존재를 하면
-                pathData.forEach((path, index) => {
-                    const listOfEdgeId = path.map(e => e.edge);
-                    const listOfNodeId = path.map(n => n.node);
-                    locaArray.push(listOfNodeId[0]);
-                    const crsFilter = makeCrsFilter(listOfEdgeId);
-                    if (index === pathData.length - 1) {
-                        locaArray.push(listOfNodeId[listOfNodeId.length - 1]);
-                    }
-                    const shortestPathLayer = new TileLayer({
-                        title: `UOS Shortest Path ${index + 1}`,
-                        source: new TileWMS({
-                            url: 'http://localhost:8080/geoserver/gp/wms',
-                            params: { 'LAYERS': 'gp:link', ...crsFilter },
-                            serverType: 'geoserver',
-                            visible: true,
-                        }),
-                        zIndex: 2
-                    });
-                    map.addLayer(shortestPathLayer);
-                    
-                });
+                addShortestPathLayer(pathData);
+                locaArray = makelocaArrayFromNodes(pathData,locaArray); // pathData 가공해서 locaArray 도출
+
                 console.log(locaArray);
             }
             // 출발, 도착, 경유 노드 표시
             if (locaArray && locaArray.length >= 2) {
-                locaArray.forEach((nodeId, index) => {
-                    let poiStyle;
-                     if (index === 0) { // 출발지
-                        poiStyle = poiStyleS;
-                    } else if (index === locaArray.length - 1) { //도착지
-                        poiStyle = poiStyleE;
-                    } else {
-                        poiStyle = poiStyle2; // 경유지
-                    }
-                    //const crsFilter = makeCrsFilter4node(locaArray);
-                    /*const shortestPathLayer = new TileLayer({
-                        title: `Marker ${index + 1}`,
-                        source: new TileWMS({
-                            url: 'http://localhost:8080/geoserver/gp/wms',
-                            params: { 'LAYERS': 'gp:node', ...crsFilter },
-                            serverType: 'geoserver',
-                            visible: true,
-                        }),
-                        zIndex: 5,
-                        style: poiStyle,
-                    });
-                    */
-
-                    // shortestPathLayer을 WMS방식에서 WFS로 변경. 아래 poiSurce랑 poiLayer 코드에서 수정.
-                    const shortestPathLayer = new VectorLayer({
-                        title: `Marker ${index + 1}`,
-                        visible: true,
-                        source: new VectorSource({ // feature들이 담겨있는 vector source
-                            format: new GeoJSON({
-                                dataProjection: 'EPSG:5181'
-                            }),
-                            url: function(extent) {
-                                return 'http://localhost:8080/geoserver/gp/wfs?service=WFS&version=2.0.0' +
-                                    '&request=GetFeature&typeName=gp%3Anode&maxFeatures=50&outputFormat=application%2Fjson&CQL_FILTER=node_id='+nodeId;
-                            },
-                            serverType: 'geoserver'
-                        }),
-                        style: poiStyle,
-                        zIndex: 5
-                    });
-
-                    map.addLayer(shortestPathLayer);
-                })
+                markNodesFrom(locaArray);
+                markerClickEventWith(locaArray); // 노드 마커 클릭 이벤트
             }
 
             if (keyword) {
@@ -241,45 +264,17 @@ const MapC = ({ pathData, width, height, keyword }) => {
                     },
                     serverType: 'geoserver'
                 });
+
                 // poiLayer -> makerLayer (이름 변경)
                 const markerLayer = new VectorLayer({
                     title: 'POI',
                     visible: true,
                     source: poiSource,
-                    style: poiStyle2,
+                    style: basicMarkerStyle(irumarker2),
                     zIndex: 4
                 });
                 map.addLayer(markerLayer)
 
-                // 수아 추가
-                // feature 클릭 가능한 select 객체
-                let selectSingleClick = new Select({
-                    condition: click, // click 이벤트. condition: Select 객체 사용시 click, move 등의 이벤트 설정
-                    style: new Style({
-                        stroke: new Stroke({
-                            color: 'white',
-                            width: 3
-                        }),
-                        fill: new Fill({
-                            color: 'rgba(0,0,255,0.6)'
-                        })
-                    })
-                });
-                map.addInteraction(selectSingleClick); // map 객체에 Select 객체 추가
-                
-                // 기존 색상을 담는 객체
-                let _style = null;
-                // feature를 선택할 때 이벤트
-                selectSingleClick.on('select', function(e) {
-                    console.log('click');
-                    /*// 이전에 선택한 feature가 있을 경우
-                    if(e.target.getFeatures()!== undefined){
-                        _style = e.target.getFeatures.a[0]; // 기존 색상 담기
-                        _style.setStyle(null); // 기존 색상 제거
-                    } else{ // 선택된 feature가 없는 경우, 이전에 선택한 feature의 스타일을 복원하고 정보를 비웁니다.
-                        _style.setStyle(poiStyle2); // 기존 색상 추가
-                    }*/
-                });
             }
         }
     }, [map, layerState, pathData, keyword]);
