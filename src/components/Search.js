de97e5b170737aef59fc51f3a103b2912a769b6a
Sini0206 <137382250+Sini0206@users.noreply.github.com>
2024-05-06 04:08:24 +0900

    feat(Search): 검색 결과가 여러 개일 시 결과 목록 표출 후 개별 클릭 시 정보창 연결
    - 기존에는 setBuildingInfo함수에서 검색 결과가 여러 개여도 오직 첫 번째 결과로만 정보창과 정보창을 구성하는 상태 변수들을 세팅했음
    - 그렇기에 setBuildingInfo 함수에서 단일 결과면 바로 세팅하고 여러 개면 resultList에 배열로 저장하여 useSearch에서 return할 수 있도록 조건문으로 구분함
    - return된 값은 SearchResultUIComponent에서 반영됨
    - resultList 내의 개별 요소들 각각 버튼화하고 이 DOM 요소에 필요한 돋보기 이미지를 MarkerStyle.js의 Icons에 변수화한 후 import 해서 씀
    - 정보창은 단일 결과일 때만 표출되도록 했기 때문에 버튼 클릭 시 candidateExist값을 false로 조작

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import {NODE_BACKEND_URL} from "../constants/urls";
 import './MapC.css';
+import {Icons} from './MarkerStyle'
 
-const searchBuildingInfo = async (keyword, setBuildingInfo, setResultExistence) => {
+const searchBuildingInfo = async (keyword, setBuildingInfo) => {
+    console.log('(searchBuildingINFO)')
     const req = {keyword};
     try {
         var response = await axios.post(NODE_BACKEND_URL+'/showBuildingInfo', req, {
@@ -11,13 +13,7 @@ const searchBuildingInfo = async (keyword, setBuildingInfo, setResultExistence) 
                 'Content-Type': 'application/json',
             },
         });
-        if (response.data.rowCount > 0) {
-            setBuildingInfo(response.data);
-            setResultExistence(true);
-        }
-        else {
-            setResultExistence(false);
-        }
+        setBuildingInfo(response.data);
     } catch (error) {
       console.error('Error during Axios POST request', error);
     }
@@ -32,29 +28,51 @@ const useSearch = (keyword) => {
     const [totalFloors, setTotalFloors] = useState(0);
     const [loungeCnt, setLoungeCnt] = useState(0);
     const [type, setType] = useState('');
+
     const [resultExistence, setResultExistence] = useState(true);
+    const [candidatesExist, setCandidatesExist] = useState(false);
+    const [resultList, setResultList] = useState([])
 
     const setBuildingInfo = (data) => {
-        setBgName(data.rows[0].bg_name)
-        setEngName(data.rows[0].eng_name)
-        setBgId(data.rows[0].bd_id)
-        setBgSummary(data.rows[0].summary)
-        setLoungeCnt(data.rows[0].lounge_count)
-        setTotalFloors(data.rows[0].total_floor)
-        setType(data.rows[0].type)
-        setBgImage(data.rows[0].image_url)
+        if (data.rowCount == 0) {           // 검색 결과가 없을 때
+            setResultExistence(false);
+        } else if (data.rowCount == 1) {    // 검색 결과가 1개일 때
+            setBgName(data.rows[0].bg_name)
+            setEngName(data.rows[0].eng_name)
+            setBgId(data.rows[0].bd_id)
+            setBgSummary(data.rows[0].summary)
+            setLoungeCnt(data.rows[0].lounge_count)
+            setTotalFloors(data.rows[0].total_floor)
+            setType(data.rows[0].type)
+            setBgImage(data.rows[0].image_url)
+       } else { // 검색 결과 후보가 여러 개일 때
+           setCandidatesExist(true)
+           const updatedResultList = [];
+           for (let i=0;i<data.rowCount;i++){
+               updatedResultList.push(data.rows[i].bg_name);
+           }
+           setResultList(updatedResultList)
+       }
     }
 
     useEffect(() => {
         //SideEffect
-        searchBuildingInfo(keyword, setBuildingInfo, setResultExistence)
+        searchBuildingInfo(keyword, setBuildingInfo)
+
+        return () => { // clean-up
+            setResultExistence(true)
+            setResultList([]);
+            setCandidatesExist(false);
+        }
     }, [keyword]) // SideEffect가 첫번째 렌더링 이후 한번 실행되고, 이후 특정 값의 업데이트를 감지했을 때마다 실행되어야 하는 경우
 
-    return {bgImage, bgName, engName, type, bgId, totalFloors, resultExistence, loungeCnt, bgSummary};
+    return {bgImage, bgName, engName, type, bgId, totalFloors, resultExistence, candidatesExist,
+    setCandidatesExist, resultList, loungeCnt, bgSummary};
 }
 
 const SearchResultUIComponent = ({keyword, setKeyword}) => {
-    const {bgImage, bgName, engName, type, bgId, totalFloors, resultExistence, loungeCnt, bgSummary} = useSearch(keyword);
+    const {bgImage, bgName, engName, type, bgId, totalFloors, resultExistence,
+    candidatesExist, setCandidatesExist, resultList, loungeCnt, bgSummary} = useSearch(keyword);
 
     if (resultExistence == false)
         return (
@@ -62,17 +80,31 @@ const SearchResultUIComponent = ({keyword, setKeyword}) => {
                 <h3> 검색 결과가 존재하지 않습니다.</h3>
             </div>
         )
-    return (
-        <div style={{margin: '0 auto'}}>
-            {bgImage && <img className="bg-image" src={bgImage} alt="Building Image" style={{width: '350px',height: '250px',display: 'block',margin: '0 auto'}}/>}
-            <div className="info-content">
-                <h3 style={{margin: '0 auto', backgroundColor: '#FFCD4A'}}> {bgName}({engName}) {<button className='button' onClick={() => setKeyword('')}>닫기</button>}</h3>
-                <div style={{fontSize: '14px', marginTop: '5px'}}> [ {type} | {(type === '건물') && (<> No.{bgId} | 총 {totalFloors}층 </>)} ] </div>
-                {loungeCnt && <>라운지 수 : {loungeCnt}</>}
-                <p style={{marginTop: '5px', marginBottom: '0px'}}> {bgSummary} </p>
+    if (candidatesExist == true) {
+        return (
+            <div>
+                {resultList.map((item, index) => (
+                    <button key={index} className = 'result-list' onClick={() => {setCandidatesExist(false); setKeyword(item)}}>
+                        <img src={Icons.magnifier} style={{width:'5%', height:'5%'}}></img>
+                        <div style={{paddingLeft: '8px'}}>{item}</div>
+                    </button>
+                ))}
             </div>
-        </div>
-    );
+        )
+    }
+    if (resultExistence && !candidatesExist) {
+        return (
+            <div style={{margin: '0 auto'}}>
+                {bgImage && <img className="bg-image" src={bgImage} alt="Building Image" style={{width: '350px',height: '250px',display: 'block',margin: '0 auto'}}/>}
+                <div className="info-content">
+                    <h3 style={{margin: '0 auto', backgroundColor: '#FFCD4A'}}> {bgName}({engName}) {<button className='button' onClick={() => setKeyword('')}>닫기</button>}</h3>
+                    <div style={{fontSize: '14px', marginTop: '5px'}}> [ {type} | {(type === '건물') && (<> No.{bgId} | 총 {totalFloors}층 </>)} ] </div>
+                    {loungeCnt && <>라운지 수 : {loungeCnt}</>}
+                    <p style={{marginTop: '5px', marginBottom: '0px'}}> {bgSummary} </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default SearchResultUIComponent;
