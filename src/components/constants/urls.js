b857a3a5b15caeb3699683ad933d36bc4c7154d7
je0seo <jeyoungseo@gmail.com>
2024-05-23 20:41:07 +0900

    geoserver_url 변경

--- src/components/constants/urls.js
+++ src/components/constants/urls.js
@@ -1,2 +1,2 @@
 export const NODE_BACKEND_URL = 'https://sinergy-backend.azurewebsites.net' //5000포트
-export const GEO_SERVER_URL = 'http://3.12.154.29:8080/geoserver'
+export const GEO_SERVER_URL = 'http://3.12.162.120:8080/geoserver' //http://3.12.154.29:8080/geoserver
